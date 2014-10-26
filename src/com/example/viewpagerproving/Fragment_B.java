6521dc40c8fa91022f2c392dc7b61cdd9db117c7
Lorena Reyes <lorenareyesp@gmail.com>
2014-10-26 17:28:56 +0100

    Created class Fragment B

--- src/com/example/viewpagerproving/Fragment_B.java
+++ src/com/example/viewpagerproving/Fragment_B.java
@@ -1,7 +1,16 @@
 package com.example.viewpagerproving;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class Fragment_B extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.fragment_b, container, false);
+	}
 
 }
